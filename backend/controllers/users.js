a001334feeb28dcfe83e5cce70f8662d743028a8
Denis Vakulenko <woof88@yandex.ru>
2022-11-08 15:21:28 +0500

    fix: headers

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -72,8 +72,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // const token = jwt.sign({ _id: user._id }, 'Yandex-the-best', { expiresIn: '7d' });
-      const key = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-      const token = jwt.sign({ _id: user._id }, key, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => next(err));
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,8 +18,7 @@ module.exports = (req, res, next) => {
 
   try {
     // payload = jwt.verify(token, 'Yandex-the-best');
-    const key = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-    payload = jwt.verify(token, key);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new IncorrectTokenError('Необходима авторизация'));
   }
