4798ca63b1e580cee2fe15a500efd1c03e41c6a0
Denis Vakulenko <woof88@yandex.ru>
2022-11-08 16:08:56 +0500

    fix: http

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,7 +2,7 @@ const settingsApi = {
   url: "https://api.vden.mesto.nomoredomains.icu",
 };
 
-const authUrl = "https://api.vden.mesto.nomoredomains.icu";
+const authUrl = "http://api.vden.mesto.nomoredomains.icu";
 
 export { settingsApi, authUrl };
 
